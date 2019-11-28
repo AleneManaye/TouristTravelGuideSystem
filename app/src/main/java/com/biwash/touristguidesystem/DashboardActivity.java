7643cfa42cf190d2984cb9abd6595e6b21a0b73a
Biwash Khatiwada <khatiwadabiwash@gmail.com>
2019-11-28 07:27:47 +0545

    Initial commit

---
+++ app/src/main/java/com/biwash/touristguidesystem/DashboardActivity.java
@@ -0,0 +1,23 @@
+package com.biwash.touristguidesystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+    }
+}
